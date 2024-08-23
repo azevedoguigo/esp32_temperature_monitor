9d4fc7b3c6a8d08b4c8bd3f83a04431c0bdbc40e
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-08-23 04:29:04 -0300

    fix: temperature decreases when the sensor heats up.

--- esp32-temperature-monitor/esp32-temperature-monitor.ino
+++ esp32-temperature-monitor/esp32-temperature-monitor.ino
@@ -1,22 +1,13 @@
-const int sensorPin = 23;
-
-const float resistor = 10000;
-const float beta = 3950;
-
 void setup() {
   Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void loop() {
-  int analogValue = analogRead(sensorPin);
+  const float BETA = 3950;
+  int analogValue = analogRead(4);
+  float celcius = 1 / (log(1 / (4095. / analogValue -1)) / BETA + 1.0 / 298.15) - 273.15;
 
-  float resistence = resistor * (4095.0 / (float)analogValue - 1.0);
-  float temperatureKelvin = 1.0 / (1.0 / 298.15 + log(resistence / resistor) / beta);
-  float temperatureCelcius = temperatureKelvin - 273.15;
-
-  Serial.print("Temperatura: ");
-  Serial.print(temperatureCelcius);
-  Serial.println(" C");
-  
-  delay(1000);
+  Serial.println(celcius);
+  delay(2000);
 }
